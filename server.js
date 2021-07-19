b08a40756f2141f5d733db096aec12a0438839ea
Jerin Ignatious Palliparambil <jerinignatious1998@gmail.com>
2021-07-19 21:32:49 +0530

    PAGE-102 | paginatedResults Added

--- server.js
+++ server.js
@@ -16,14 +16,39 @@ const users = [
     {id: "12", name: "User 12"}
 ]
 
-app.get('/users', (req, res) => {
-    const page = req.query.page
-    const limit = req.query.limit
-
-    const startIndex = (page - 1) * limit
-    const endIndex = page * limit
-    const resultUsers = users.slice(startIndex, endIndex)
-    res.json(resultUsers)
+app.get('/users', paginatedResults(users), (req, res) => {
+    res.json(res.paginatedResults)
 })
+
+function paginatedResults(model){
+    return (req, res, next) => {
+        const page = parseInt(req.query.page)
+        const limit = parseInt(req.query.limit)
+
+        const startIndex = (page - 1) * limit
+        const endIndex = page * limit
+
+        const results = {}
+
+        if(endIndex < model.length){
+            results.next = {
+                page: page + 1,
+                limit: limit
+            }
+        }
+
+        if(startIndex > 0){
+            results.previous = {
+                page: page - 1,
+                limit: limit
+            }
+        } 
+
+        
+        results.results = model.slice(startIndex, endIndex)
+        res.paginatedResults = results
+        next()
+    }
+}
 
 app.listen(3000)
