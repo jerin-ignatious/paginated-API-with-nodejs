ffb13a336c3e8e400cc0da83b65ee2e52f2f746f
Jerin Ignatious Palliparambil <jerinignatious1998@gmail.com>
2021-08-09 07:30:28 +0530

    PAGE-104 | Final commit

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const User = require('./users')
 
-const users = [
-    {id: "1", name: "User 1"},
-    {id: "2", name: "User 2"},
-    {id: "3", name: "User 3"},
-    {id: "4", name: "User 4"},
-    {id: "5", name: "User 5"},
-    {id: "6", name: "User 6"},
-    {id: "7", name: "User 7"},
-    {id: "8", name: "User 8"},
-    {id: "9", name: "User 9"},
-    {id: "10", name: "User 10"},
-    {id: "11", name: "User 11"},
-    {id: "12", name: "User 12"}
-]
+mongoose.connect('mongodb://localhost/pagination', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.once('open', async () => {
+    if(await User.countDocuments().exec() > 0) return
 
-app.get('/users', paginatedResults(users), (req, res) => {
+    Promise.all([
+        User.create({ name: 'User1' }),
+        User.create({ name: 'User2' }),
+        User.create({ name: 'User3' }),
+        User.create({ name: 'User4' }),
+        User.create({ name: 'User5' }),
+        User.create({ name: 'User6' }),
+        User.create({ name: 'User7' }),
+        User.create({ name: 'User8' }),
+        User.create({ name: 'User9' }),
+        User.create({ name: 'User10' }),
+        User.create({ name: 'User11' }),
+        User.create({ name: 'User12' })
+    ]).then(() => console.log('Added Users'))
+})
+
+app.get('/users', paginatedResults(User), (req, res) => {
     res.json(res.paginatedResults)
 })
 
 function paginatedResults(model){
-    return (req, res, next) => {
+    return async (req, res, next) => {
         const page = parseInt(req.query.page)
         const limit = parseInt(req.query.limit)
 
@@ -30,7 +38,7 @@ function paginatedResults(model){
 
         const results = {}
 
-        if(endIndex < model.length){
+        if(endIndex < await model.countDocuments().exec()){
             results.next = {
                 page: page + 1,
                 limit: limit
@@ -43,11 +51,17 @@ function paginatedResults(model){
                 limit: limit
             }
         } 
-
+        try{
+            results.results = await model.find().limit(limit).skip(startIndex).exec()
+            res.paginatedResults = results
+            next()
+        }catch(e) {
+            res.status(500).json({message: e.message})
+        }
         
-        results.results = model.slice(startIndex, endIndex)
-        res.paginatedResults = results
-        next()
+        
+        
+        
     }
 }
 
--- users.js
+++ users.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     name:{
         type: String,
-        required
+        required: true
     }
 })
 
-modules.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema)
